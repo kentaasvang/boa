52604a61ce57c8dc4f324ca678d0107412e56abd
Kent Åsvang <kentaasvang@gmail.com>
2020-09-05 12:40:33 +0200

    finished script to create setup.py

--- create.sh
+++ create.sh
@@ -2,8 +2,7 @@
 
 DEFAULT_PROJECT_FOLDER=$HOME/PythonProjects
 
-COMMAND=$1
-PROJECT_NAME=$2
+COMMAND=$1 PROJECT_NAME=$2
 PROJECT_ROOT=$DEFAULT_PROJECT_FOLDER/$PROJECT_NAME
 CURRENT_YEAR=$(date +"%Y")
 
@@ -40,6 +39,7 @@ function create_project_files_and_folders() {
 	echo "Creating project files and folders... "
 	create_readme	
 	create_license
+	create_setup_py
 }
 
 function create_readme() {
@@ -56,7 +56,7 @@ function create_license() {
 Copyright (c) $CURRENT_YEAR $USER
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
+of this software and associated documentation files (the \"Software\"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
@@ -65,13 +65,36 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE." > $PROJECT_ROOT/LICENSE
+}
+
+function create_setup_py() {
+	echo "Creating setup.py... "
+	touch $PROJECT_ROOT/setup.py
+	echo "from setuptools import setup, find_packages
+
+with open(\"README.md\", \"r\") as file_handler:
+    long_description = file_handler.read()
+
+setup(
+    name=\"$PROJECT_NAME\",
+    version=\"0.0.1\",
+    author=\"$USER\",
+    long_description=long_description,
+    long_description_content_type=\"text/markdown\",
+    packages=find_packages(),
+    classifiers=[
+        \"Programming Language :: Python :: 3\",
+        \"License :: OSI Approved :: MIT License\",
+    ],
+)
+" > $PROJECT_ROOT/setup.py
 }
 
 
