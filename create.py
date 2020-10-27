e4b144a3a11fc666a77ab661eb44ae54de978b87
Kent Åsvang <kentaasvang@gmail.com>
2020-10-27 08:50:12 +0100

    added small templating-engine for files

--- create.py
+++ create.py
@@ -9,108 +9,7 @@ import getpass
 
 from argparse import ArgumentParser
 import settings
-
-
-default_project_dir = os.getcwd()
-project_name = parse_command_line_arguments()
-project_root = default_project_dir + "/" + project_name
-current_year = datetime.datetime.now().year
-current_user = getpass.getuser()
-
-LICENSE_CONTENT = """MIT License
-
-Copyright (c) {} {}
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the \"Software\"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.""".format(current_year, current_user)
-
-SETUP_CONTENT =	"""from setuptools import setup, find_packages
-
-with open(\"README.md\", \"r\") as file_handler:
-    long_description = file_handler.read()
-
-setup(
-    name=\"{}\",
-    version=\"0.0.1\",
-    author=\"{}\",
-    long_description=long_description,
-    long_description_content_type=\"text/markdown\",
-    packages=find_packages(),
-    classifiers=[
-        \"Programming Language :: Python :: 3\",
-        \"License :: OSI Approved :: MIT License\",
-    ],
-)\n""".format(project_name, current_user)
-
-
-SETTINGS_CONTENT = """import os
-
-DEVELOPMENT = True
-
-
-filename = \".env.dev\" if DEVELOPMENT else \".env\"
-
-# Read in key/values from environment file and load
-# them into the enrironment
-try:
-    with open(filename, \"r\") as file_handler:
-        arguments = []
-
-        # read lines in file and append to arguments-list
-        while argument := file_handler.readline():
-            arguments.append(argument.split(\"=\"))
-
-        # load key/values into environment so they can be reached with
-        # os.getenv()
-        for key, value in arguments:
-            os.environ[key] = value
-
-except FileNotFoundError as file_not_found:
-    print(f\"The file {filename} was not found. Make sure it's created\")
-
-"""
-
-MAIN_CONTENT="""def main():
-    return \"hello, world\"
-
-if __name__ == \"__main__\":
-    main()
-"""
-
-TEST_CONTENT="""import unittest
-import context
-import {}
-
-class Test{}(unittest.TestCase):
-    def test_{}(self):
-        self.assertEqual({}.main(), \"hello, world\")
-
-if __name__ == \"__main__\":
-    unittest.main()
-""".format(project_name, project_name, project_name, project_name, project_root)
-
-CONTEXT_CONTENT="""\"\"\" Fixing the import path for importing modules from root \"\"\"
-
-import os, sys
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-)
-"""
-
+import templates
 
 
 def create_file(file_path, file_name, file_content):
@@ -130,23 +29,23 @@ def create_project_files_and_folders():
     # Create README.md
     create_file(project_root, "README.md", "# " + project_name)
     # Create LICENSE
-    create_file(project_root, "LICENSE", LICENSE_CONTENT)
+    create_file(project_root, "LICENSE", LICENSE)
     # Create setup.py
-    create_file(project_root, "setup.py", SETUP_CONTENT)
+    create_file(project_root, "setup.py", SETUP)
     # Create settings.py
-    create_file(project_root, "settings.py", SETTINGS_CONTENT)
+    create_file(project_root, "settings.py", SETTINGS)
     # Create .env and .env.dev
     create_file(project_root, ".env", "")
     create_file(project_root, ".env.dev", "")
     # Create requirements.txt
     create_file(project_root, "requirements.txt", "")
     # Create main_module
-    create_file(project_root, project_name + '.py', MAIN_CONTENT)
+    create_file(project_root, project_name + '.py', MAIN)
     # Create test packages
     os.mkdir(project_root + "/tests")
-    create_file(project_root + "/tests", "test_main.py", TEST_CONTENT)
+    create_file(project_root + "/tests", "test_main.py", TEST)
     # context.py
-    create_file(project_root + "/tests", "context.py", CONTEXT_CONTENT)
+    create_file(project_root + "/tests", "context.py", CONTEXT)
     # Create Makefile
     create_file(project_root, "Makefile", "test:\n\tpython3 tests/test_main.py")
     # Create gitignore
@@ -179,6 +78,46 @@ def parse_command_line_arguments() -> str:
     return parser.parse_args().name
 
 
+def template_engine(template: str, data: dict) -> str:
+    """
+    Takes a template and a dict and insert the values
+    in the dict to the corresponding keys in a template
+
+    Example:
+        template_engine("hello, (( name ))", {"name": "world"})
+        returns: "hello, world" 
+    """
+    for key, value in data.items():
+        template = template.replace(f"(( {key} ))", str(value))
+    return template
+
+
+class File:
+    name: str
+    content: str
+
+
 if __name__ == "__main__":
+    default_project_dir = os.getcwd()
+    project_name = parse_command_line_arguments()
+    project_root = default_project_dir + "/" + project_name
+    current_year = datetime.datetime.now().year
+    current_user = getpass.getuser()
+
+    LICENSE = template_engine(
+        templates.LICENSE, {"name": project_name, "year": current_year})
+
+    SETUP = template_engine(
+        templates.SETUP, {"name": project_name, "user": current_user})
+
+    SETTINGS = templates.SETTINGS
+
+    MAIN = templates.MAIN
+
+    TEST = template_engine(templates.TEST, {"name": project_name})
+
+    CONTEXT = templates.CONTEXT
+    
+
     create()
 
