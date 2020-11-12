8b921ed10c9222cb096b57eca5d9f3cda547d4b2
Kent Åsvang <kentaasvang@gmail.com>
2020-11-12 10:31:49 +0100

    some refactoring to make the code more DRY

--- create.py
+++ create.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 from os.path import expanduser
 from os import path, mkdir
 import os
@@ -11,57 +10,45 @@ from argparse import ArgumentParser
 import settings
 import templates
 
+from typing import Dict
 
-def create_file(file_path, file_name, file_content):
-    print("Creating ", file_name, "...")
-    file = open(file_path + "/" + file_name, "w")
-    file.write(file_content + "\n")
-    file.close()
 
-def create_project_folder():
-    print(project_root)
+def create_file(directory: str, name: str, content: str) -> None:
+    """
+    Creates a file in the given directory and fills with given content
+    """
+    with open(f"{directory}/{name}", "w") as f:
+        f.write(f"{content}\n")
+
+
+def create_project_folder(project_root: str) -> None:
+    """
+    Creates the project root folder
+    """
     if not path.isdir(project_root):
-        print("Creating project folder...")
         os.mkdir(project_root)
 
-def create_project_files_and_folders():
-    print("Creating project files and folders...")
-    # Create README.md
-    create_file(project_root, "README.md", "# " + project_name)
-    # Create LICENSE
-    create_file(project_root, "LICENSE", LICENSE)
-    # Create setup.py
-    create_file(project_root, "setup.py", SETUP)
-    # Create settings.py
-    create_file(project_root, "settings.py", SETTINGS)
-    # Create .env and .env.dev
-    create_file(project_root, ".env", "")
-    create_file(project_root, ".env.dev", "")
-    # Create requirements.txt
-    create_file(project_root, "requirements.txt", "")
-    # Create main_module
-    create_file(project_root, project_name + '.py', MAIN)
-    # Create test packages
-    os.mkdir(project_root + "/tests")
-    create_file(project_root + "/tests", "test_main.py", TEST)
-    # context.py
-    create_file(project_root + "/tests", "context.py", CONTEXT)
-    # Create Makefile
-    create_file(project_root, "Makefile", "test:\n\tpython3 tests/test_main.py")
-    # Create gitignore
-    create_file(project_root, ".gitignore", "# vim files\n*.swp\n*.swo\n# python cache\n__pycache__/\n# prod-files\n.env") 
 
-def git_initial_commit():
+def create_project_files_and_folders(root: str, files: Dict[str, str]) -> None:
+    """
+    Creates all the project files that are needed
+    """
+    for name, content in files.items():
+        create_file(root, name, content)
+
+    # Create Makefile
+    create_file(root, "Makefile", "test:\n\tpython3 tests.py")
+    # Create gitignore
+    create_file(root, ".gitignore", "# vim files\n*.swp\n*.swo\n# python cache\n__pycache__/\n# prod-files\n.env") 
+
+
+def git_initial_commit() -> None:
+    """
+    Sets up git in project root
+    """
     print("git init and commit!...")
     os.system("git init --quiet && git add . && git commit --quiet -m \"initial commit\"");
 
-def create():
-    if not path.isdir(default_project_dir):
-        print("Creating default project folder...")
-        os.mkdir(default_project_dir)
-
-    create_project_folder()
-    create_project_files_and_folders()
 
 
 def parse_command_line_arguments() -> str:
@@ -92,12 +79,10 @@ def template_engine(template: str, data: dict) -> str:
     return template
 
 
-class File:
-    name: str
-    content: str
-
-
-if __name__ == "__main__":
+def create() -> None:
+    """
+    Program entrypoint
+    """
     default_project_dir = os.getcwd()
     project_name = parse_command_line_arguments()
     project_root = default_project_dir + "/" + project_name
@@ -116,8 +101,21 @@ if __name__ == "__main__":
 
     TEST = template_engine(templates.TEST, {"name": project_name})
 
-    CONTEXT = templates.CONTEXT
-    
+    if not path.isdir(default_project_dir):
+        os.mkdir(default_project_dir)
 
+    files = {
+        "LICENSE": LICENSE, 
+        "setup.py": SETUP, 
+        "settings.py": SETTINGS, 
+        f"{project_name}.py": MAIN, 
+        "tests.py": TEST, 
+    }
+
+    create_project_folder(project_name)
+    create_project_files_and_folders(project_root, files)
+
+
+if __name__ == "__main__":
     create()
 
