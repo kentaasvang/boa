d6ec8c62f442b45e0a70ceb31c8ff68f1ab72fec
Kent Åsvang <kentaasvang@gmail.com>
2020-11-12 15:15:17 +0100

    updated github-repo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email="kentaasvang@gmail.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kentaasvang/python_create",
+    url="https://github.com/kentaasvang/boa",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.8",
