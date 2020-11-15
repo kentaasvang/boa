a53d7b22b7d8eab72872d526bb6402c5c5a069a9
Kent Åsvang <kentaasvang@gmail.com>
2020-11-15 12:34:38 +0100

    not using Makefile anymore. Make.py

--- command_line.py
+++ command_line.py
@@ -14,3 +14,13 @@ def new(name):
     Creates a new project NAME
     """
     boa.new(p_name=name)
+
+
+@cli.command()
+@click.argument("command")
+def make(command):
+    try:
+        import make
+        getattr(make, command)()
+    except AttributeError as attribute_error:
+        click.echo("There is no command in make.py with the name %s" % command)
