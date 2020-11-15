c1d123359f9f81249bdce274e5d2f49437b07854
Kent Åsvang <kentaasvang@gmail.com>
2020-11-15 12:11:54 +0100

    refactored cleaner API for boa-package

--- boa/boa.py
+++ boa/boa.py
@@ -80,7 +80,7 @@ def template_engine(template: str, data: dict) -> str:
     return template
 
 
-def boa(p_name: str = None, test: bool = False, p_root: str = "") -> None:
+def new(p_name: str = None, test: bool = False, p_root: str = "") -> None:
     """
     Program entrypoint
 
