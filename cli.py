7513278d5b2151a18e963ed01744e2d7adb2b92e
Kent Åsvang <kentaasvang@gmail.com>
2021-02-21 11:12:18 +0100

    command_line.py -> cli.py

--- cli.py
+++ cli.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import click
 
 from boa import boa
--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as file_handler:
     long_description = file_handler.read()
 
+
 setup(
     name="pboa",
     version="0.0.1a1",
@@ -19,8 +20,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Environment :: Console",
         "Operating System :: OS Independent"],
-    entry_points= {
-        # "console_scripts": ["boa=boa.boa:boa"]},
-        "console_scripts": ["boa=command_line:cli"]},
+    entry_points={"console_scripts": ["boa=cli:cli"]},
     python_requires=">=3.8"
 )
